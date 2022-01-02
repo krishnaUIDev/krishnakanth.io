f958cb2e46c1186a408659aea0adbe8722c4b874
krishnaUIDev <krishna.kondoju007@gmail.com>
2022-01-02 08:56:04 -0500

    added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import type { AppProps } from "next/app";
+import { NextIntlProvider } from "next-intl";
+
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+const progrss = new ProgressBar({
+  size: 4,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progrss.start);
+Router.events.on("routeChangeComplete", progrss.finish);
+Router.events.on("routeChangeError", progrss.finish);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextIntlProvider messages={pageProps.messages}>
+      <Component {...pageProps} />
+    </NextIntlProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
