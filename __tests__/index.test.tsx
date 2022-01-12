19f085c99fd8f1d361ddb994d0c4a0d47ff7ce52
krishnaUIDev <krishna.kondoju007@gmail.com>
2022-01-11 23:34:30 -0500

    Added

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,5 +2,8 @@ import { render, screen } from "@testing-library/react";
 import Home from "../pages/index";
 
 describe("Home", () => {
-  it("renders a heading", () => {});
+  it("renders a heading", () => {
+    const container = render(<Home />);
+    expect(container).toMatchSnapshot();
+  });
 });
