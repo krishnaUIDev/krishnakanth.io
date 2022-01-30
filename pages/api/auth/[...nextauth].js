b4124f08a46a2727083569161c1f8ffdde6695f6
krishnaUIDev <krishna.kondoju007@gmail.com>
2022-01-29 21:42:57 -0500

    Added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,12 @@
-import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import NextAuth from "next-auth";
 
 const prisma = new PrismaClient();
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
