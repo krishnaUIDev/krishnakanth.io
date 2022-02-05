472fb66ad4def1bc25bbd049e9f2e50092e89121
krishnaUIDev <krishna.kondoju007@gmail.com>
2022-02-04 23:01:50 -0500

    removed prisma

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,5 @@
 import GithubProvider from "next-auth/providers/github";
-// import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
-//import prisma from "../../../prisma";
-// import { PrismaClient } from "@prisma/client";
-// const prisma = new PrismaClient();
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
 
